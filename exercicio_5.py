8a828ad4d5c0ef177acd8699b076d26f275909e6
Dani-1708 <daniela.cris.godoy@gmail.com>
2026-05-21 23:02:00 -0300

    Update exercicio_5.py

--- exercicio_5.py
+++ exercicio_5.py
@@ -41,29 +41,30 @@ lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], 
 
 # Fazer a partir daqui
 
-lista_de_organismos = [
-    [50, 50, 50],
-    [125, 99, 12],
-    [19, 91, 42],
-    [40, 189, 0],
-    [1, 0, 0],
-    [100, 100, 70],
-    [99, 12, 12]
-]
+lista_de_organismos = [[100, 200, 300], [1, 99, 10000], [1, 1, 1]]
+
+
 maior_media = 0
 posicao_maior = 0
 
+
 for i in range(len(lista_de_organismos)):
-    
+
     organismo = lista_de_organismos[i]
-soma = 0
-for valor in organismo:
+
+    soma = 0
+
+    
+    for valor in organismo:
         soma += valor
 
-media = soma / len(organismo)
 
-if media > maior_media:
+    media = soma / len(organismo)
+
+    
+    if media > maior_media:
         maior_media = media
         posicao_maior = i
+
 
 print(f"O organismo com maior média é o da posição {posicao_maior} da lista.")
